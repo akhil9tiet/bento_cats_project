11cc97a64dd36cce2b216906012856a064d7f261
Akhil <akhil9tiet@gmail.com>
2020-02-28 20:54:33 -0800

    wip: remove errors

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 
 import './App.css';
-var data = [
+const data = [
 	{
 		id: 1,
 		url: 'https://25.media.tumblr.com/tumblr_mcpa8zbbiZ1qze0hyo1_500.jpg',
@@ -30,32 +30,32 @@ var data = [
 		favorite: false,
 	},
 	{
-		id: 1,
+		id: 4,
 		url: 'https://25.media.tumblr.com/tumblr_m2y7ezQvl81rnn1i7o1_500.jpg',
 		fact: 'A happy cat holds her tail high and steady.',
 		favorite: false,
 	},
 	{
-		id: 1,
+		id: 5,
 		url: 'https://s3.us-west-2.amazonaws.com/cdn2.thecatapi.com/images/80i.gif',
 		fact: "The cat's footpads absorb the shocks of the landing when the cat jumps.",
 		favorite: false,
 	},
 	{
-		id: 4,
+		id: 6,
 		url: 'https://s3.us-west-2.amazonaws.com/cdn2.thecatapi.com/images/FwgiToYPs.jpg',
 		fact:
 			'Cats don’t have sweat glands over their bodies like humans do. Instead, they sweat only through their paws.',
 		favorite: false,
 	},
 	{
-		id: 5,
+		id: 7,
 		url: 'https://s3.us-west-2.amazonaws.com/cdn2.thecatapi.com/images/GpaWAyWT6.jpg',
 		fact: 'The first cat show was organized in 1871 in London. Cat shows later became a worldwide craze.',
 		favorite: false,
 	},
 	{
-		id: 6,
+		id: 8,
 		url: 'https://s3.us-west-2.amazonaws.com/cdn2.thecatapi.com/images/CfvDVOap6.jpg',
 		fact:
 			'The lightest cat on record is a blue point Himalayan called Tinker Toy, who weighed 1 pound, 6 ounces (616 g). Tinker Toy was 2.75 inches (7 cm) tall and 7.5 inches (19 cm) long.',
@@ -65,29 +65,56 @@ var data = [
 const App = () => {
 	const [hash, setHash] = useState(data);
 
-	var homeData = [],
-		favData = [];
-	useEffect(() => {
-		homeData = hash.filter((d) => !d.favorite);
-		favData = hash.filter((d) => d.favorite);
-		setHash([...favData, ...homeData]);
-	}, []);
-	console.log(hash);
+	// var homeData = [],
+	//   favData = [];
+
+	// useEffect(() => {
+	// 	homeData = hash.filter((d) => !d.favorite);
+	// 	favData = hash.filter((d) => d.favorite);
+	// 	setHash([...favData, ...homeData]);
+	// }, []);
+
 	return (
 		<React.Fragment>
 			<h1>Bento Cats Project</h1>
+      
 			<Grid container style={{ paddingLeft: 60, paddingTop: 40, paddingRight: 60 }} spacing={2}>
 				{hash.map((card, i) => {
 					return (
 						<Grid item xs={12} sm={6} md={4} lg={3} key={card.id}>
-							<Card key={card.id}>
-								<CardContent key={card.id}>
-									<CardMedia key={card.id} style={{ height: 450 }} image={card.url} title={card.id} />
-									<Typography key={card.id} variant='body2' component='p'>
+							<Card >
+								<CardContent >
+									<CardMedia style={{ height: 450 }} image={card.url} title={card.id} />
+									<Typography variant='body2' component='p'>
 										{card.fact}
 									</Typography>
-									<IconButton aria-label='add to favorites'>
-										<FavoriteIcon key={card.id} color={card.favorite ? 'secondary' : 'default'} />
+									<IconButton
+										
+										onClick={() => {
+											const restD = hash.filter((el) => el.id !== card.id);
+											console.log('restD', restD);
+											card.favorite
+												? setHash([
+														...restD,
+														{
+															id: card.id,
+															url: card.url,
+															fact: card.fact,
+															favorite: !card.favorite,
+														},
+												  ])
+												: setHash([
+														{
+															id: card.id,
+															url: card.url,
+															fact: card.fact,
+															favorite: !card.favorite,
+														},
+														...restD,
+												  ]);
+										}}
+										aria-label='add to favorites'>
+										<FavoriteIcon  color={card.favorite ? 'secondary' : 'primary'} />
 									</IconButton>
 								</CardContent>
 							</Card>
